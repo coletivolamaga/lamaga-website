d916a738e3dba9c17b64eeaa9d542c02a4363fe0
coletivolamaga <89660217+coletivolamaga@users.noreply.github.com>
2021-08-29 13:38:54 +0100

    Add files via upload

--- scripts/data.js
+++ scripts/data.js
@@ -8,57 +8,12 @@ var data = [
         type: 'Comentário',
         date: 'October 12, 2019 00:00:00',
         tags: ['tag', 'oi', 'vaquinhas'],
-        thumbnail: 'kkk.jpg',
+        thumbnail: '',
         text: "<p><i>Just the Wind</i> is a magazine all about contrasts, the duality of two separate ends, the wind and the storm. The name Just the Wind came from a Magic the Gathering™ card with the same name. The card name comes to show a moment where we ask &quot;what was it?&quot; and respond with: &quot;nothing, just the wind&quot;.<br><br>This first issue is about humanity and its next stages, how to evolve a society and how we could reach the pinnacle of the human condition. <a href='#'>The human being</a> as the center of all the universe, or the possibility that of not ever being...<br><br>With the Wind and the Storm, we want to create a blur between the real insignificance and the insignificance of the real, revealing the big picture as a drama but always keeping in mind that, like it says in the card’s flavor text, &quot;There’s nothing to worry about&quot;.</p>",
         notes: [],
         collaboration: ['oi', 'lenha'],
         images: ["kkk.jpg", 'supreme.png', 'favicon.png'],
         misc: ['<iframe width="560" height="315" src="https://www.youtube.com/embed/T1zQ5b3C1hM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']
-    },
-    {
-        title_home: 'Teste Item1',
-        title_full: '',
-        subtitle: '',
-        author: 'Raquel Espadinha',
-        type: 'Comentário',
-        date: 'October 12, 2019 00:00:00',
-        tags: ['kkk', 'oi'],
-        thumbnail: 'supreme.png',
-        text: "<p><i>Just the Wind</i> is a magazine all about contrasts, the duality of two separate ends, the wind and the storm. The name Just the Wind came from a Magic the Gathering™ card with the same name. The card name comes to show a moment where we ask &quot;what was it?&quot; and respond with: &quot;nothing, just the wind&quot;.<br><br>This first issue is about humanity and its next stages, how to evolve a society and how we could reach the pinnacle of the human condition. The human being as the center of all the universe, or the possibility that of not ever being...<br><br>With the Wind and the Storm, we want to create a blur between the real insignificance and the insignificance of the real, revealing the big picture as a drama but always keeping in mind that, like it says in the card’s flavor text, &quot;There’s nothing to worry about&quot;.</p>",
-        notes: [],
-        collaboration: [],
-        images: ['kkk.jpg'],
-        misc: ['<iframe src="https://player.vimeo.com/video/216278745" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> <p><a href="https://vimeo.com/216278745">DJ Hell - Car Car Car</a> from <a href="https://vimeo.com/gigolorecords">Gigolo Records</a> on <a href="https://vimeo.com">Vimeo</a>.</p>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/T1zQ5b3C1hM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']
-    },
-    {
-        title_home: 'Crónica para um',
-        title_full: '',
-        subtitle: '',
-        author: 'Francisca Portugal',
-        type: 'Crónica',
-        date: 'April 5, 2021 00:00:00',
-        tags: ['kkk', 'oi'],
-        thumbnail: '',
-        text: '<p>Cara Francisca, escrevo-lhe para a pôr a par de algumas teses e conclusões a que cheguei nos últimos dias. Espero, como combinado anteriormente, que mantenhamos sigilo nesta nossa correspondência, pois caso estas palavras caiam nas mãos erradas, toda a minha reputação ficaria manchada! ARGH, que desastre seria! A verdade é que estou muito consternada e quero confessar a minha frustração relativamente à minha produção escrita. E mais! Ao uso ingrato da palavra conteúdo e os seus critérios de partilha.<br></p><p>Bem, começando pelo princípio, todo este rodopio surgiu de uma discussão em que um editor de uma revista de renome internacional contava a dificuldade de escoar uma revista impressa para o mercado de papelarias e livrarias num momento de limitação das fronteiras e serviços de correios limitados ao essencial. É verdade que uma revista sobre arte contemporânea não é a última coca- cola do deserto em tempos de colapso financeiro, político e social mundialmente. Esta realidade fez, contudo, o tal editor revelar não só a fragilidade de um periódico físico mas também as implicações de noticiar e encomendar material online se mesmo assim ninguém a vai ler. Fiquei embasbacada com tal declaração! Como assim? O que quer dizer com NINGUÉM? Será uma hipérbole da estatística, mas percebi, pelo seu discurso, um certo ceticismo no chamado conteúdo online e na constante repetição de temas e assuntos publicados. O que é que este senhor queria com esta conversa? Talvez uma ideia vinda dos ouvintes que revolucionasse a sua produção editorial. Não descordo, aliás, depois de desligar a video chamada fiquei hipersensível com qualquer conteúdo. BLHARK! Fez-me refletir sobre a utilidade ou mesmo o critério das publicações que caiem no meu telemóvel: um gato, um peixe, um almoço, umas botas novas. E todo este trabalho para quem? Para que olhos ou cabeças? Toda a gente vê mas não quer dizer que interesse, ya? Aqui reside uma referecia útil à distinção de John Berger entre “gazing” e “seeing”.<br></p><p>A., com quem esgrimo todos estes problemas, tem dois argumentos não muito úteis para este assunto: um a meu favor e outro que provoca. Sendo, que, e isto talvez seja importante apontar que também faço parte destes ciclos da produção digital, contribuindo exponencialmente para uma revista de arte com entrevistas e artigos de opinião. Voltando ao lado positivo, é a leitura da crónica de Manuel António Pina que re-lê um jornal antigo e que põe à prova a memória dos acontecimentos e consequentemente a utilidade de produzir um jornal diário . O segundo, que 1 vejo como uma palmadinha nas costas, é a simpatética hipótese de ALGUÉM passar os olhos, ler na diagonal as palavras que publico. QUEM, então, realmente lê o que escrevo? Incluindo esta crónica... Talvez só eu, por escrevê-la e por endereçar-la para mim mesma.<br></p>',
-        notes: ['“E eu penso: talvez, afinal, alguma forma efémera eternidade possa (quem sabe?) animar a furtiva vida de 1 um jornal, um breve reflexo, uma fugaz emoção, possa, com um pouco de sorte, pulsar na notícia, na reportagem, na crónica e, por um instante, bater, unanimemente no coração de outro homem, uma anónima identidade misteriosamente gerar-se entre desconhecidos pelo milagre de um verbo ou de um adjectivo, um clandestino sangue transbordar da página impressa e contagiar a inúmera vida que, lá fora, incertamente vive.” (JN, 02.09.1992)', '“E eu penso: talvez, afinal, alguma forma efémera eternidade possa (quem sabe?) animar a furtiva vida de 1 um jornal, um breve reflexo, uma fugaz emoção, possa, com um pouco de sorte, pulsar na notícia, na reportagem, na crónica e, por um instante, bater, unanimemente no coração de outro homem, uma anónima identidade misteriosamente gerar-se entre desconhecidos pelo milagre de um verbo ou de um adjectivo, um clandestino sangue transbordar da página impressa e contagiar a inúmera vida que, lá fora, incertamente vive.” (JN, 02.09.1992)'],
-        collaboration: [],
-        images: [],
-        misc: ['']
-    },
-    {
-        title_home: 'Frente de Sala',
-        title_full: '',
-        subtitle: '',
-        author: 'Afonso Matos',
-        type: 'Coluna',
-        date: 'April 5, 2021 00:00:00',
-        tags: ['kkk', 'oi'],
-        thumbnail: 'favicon.png',
-        text: 'sadsasdasdasd<sup>2</sup>',
-        notes: [],
-        collaboration: [],
-        images: [],
-        misc: ['<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/987522493&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/iamzxzx" title="zxzx" target="_blank" style="color: #cccccc; text-decoration: none;">zxzx</a> · <a href="https://soundcloud.com/iamzxzx/give-me-some-space-to-dance-vol-3" title="give me some space to dance vol 3" target="_blank" style="color: #cccccc; text-decoration: none;">give me some space to dance vol 3</a></div>']
     }
 
     //sleciona e cola
